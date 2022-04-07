2173f00810b62594e516ea2c6ef874139b114212
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-04-07 16:12:53 -0400

    Finished

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -24,7 +26,6 @@ public class HamletParser {
                 result.append(line).append("\n");
             }
 
-            scanner.close();
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -34,6 +35,79 @@ public class HamletParser {
 
     public String getHamletData(){
         return hamletData;
+    }
+
+    public String ChangeHamletToLeon(){
+
+        if(!FindHoratio(hamletData)) return hamletData;
+        return replace_word("Hamlet","Leon");
+    }
+
+    public String ChangeHoratioToTariq(){
+
+        if(!FindHoratio(hamletData)) return hamletData;
+        return replace_word("Horatio","Tariq");
+    }
+
+    public boolean FindHoratio(String text){
+        String regex = ".*Horatio.*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.find();
+    }
+
+    public boolean FindHamlet(String text) {
+        String regex = ".*Hamlet.*";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(text);
+
+        return matcher.find();
+    }
+
+    public String replace_word(String old_word, String new_word){
+
+        String text = hamletData+ " ";
+        String regex = old_word;
+        String new_string = "";
+
+        Pattern pattern = Pattern.compile(regex);
+
+        char c;
+        String word = "";
+
+        for(int i=0; i<text.length(); i++){
+            c = text.charAt(i);
+
+            if(c==' ' || c=='\n'){
+                Matcher matcher = pattern.matcher(word);
+                while(matcher.find()){
+                    word = replace(word, new_word, matcher.start(), matcher.end());
+                }
+
+                if(c=='\n') word+='\n';
+                else if(i != text.length()-1)word+= " ";
+                new_string += word;
+
+                word = "";
+            }else{
+                word += c;
+            }
+        }
+
+        return new_string;
+    }
+
+    public String replace(String original, String toBeReplaced, int startIndex, int endIndex){
+        String first = "";
+        for(int j =0; j<startIndex; j++){
+            first += original.charAt(j);
+        }
+        String second ="";
+        for(int j =endIndex; j<original.length(); j++){
+            second += original.charAt(j);
+        }
+        return first+toBeReplaced+second;
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -15,17 +15,65 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String expected = hamletText.replaceAll("Hamlet","Leon");
+        String actual = hamletParser.ChangeHamletToLeon();
+
+        assertEquals(expected, actual);
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String expected = hamletText.replaceAll("Horatio","Tariq");
+        String actual = hamletParser.ChangeHoratioToTariq();
+
+        assertEquals(expected, actual);
+
     }
 
     @Test
-    public void testFindHoratio() {
+    public void testFindHoratio1() {
+        String text = "Horatio says 'tis but our fantasy";
+        boolean expected = true;
+        boolean actual = hamletParser.FindHoratio(text);
+
+        assertEquals(actual,expected);
     }
 
     @Test
-    public void testFindHamlet() {
+    public void testFindHoratio2(){
+        String text = "HORATIO Well, sit we down";
+        boolean expected = false;
+        boolean actual = hamletParser.FindHoratio(text);
+
+        assertEquals(actual,expected);
     }
+
+    @Test
+    public void testFindHamlet1() {
+        String text = "The Tragedy of Hamlet, Prince of Denmark";
+        boolean expected = true;
+        boolean actual = hamletParser.FindHamlet(text);
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testFindHamlet2() {
+        String text = "Shakespeare homepage | Hamlet | Entire play";
+        boolean expected = true;
+        boolean actual = hamletParser.FindHamlet(text);
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void testFindHamlet3() {
+        String text = "Shakespeare homepage | HAMLET  | Entire play";
+        boolean expected = false;
+        boolean actual = hamletParser.FindHamlet(text);
+
+        assertEquals(actual,expected);
+    }
+
 }
